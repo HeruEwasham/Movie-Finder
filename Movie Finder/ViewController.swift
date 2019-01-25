3835d3ca1c6bae44809c9af1b3bb5210ca375f5a
Yngve Hestem <herdon@me.com>
2019-01-25 17:33:41 +0100

    Initial Commit

---
+++ Movie Finder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie Finder
+//
+//  Created by Yngve Hestem on 25/01/2019.
+//  Copyright © 2019 Yngve Hestem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
